1baaf3078a53993d2d877df1b4383d0a212ec863
Alexey Pakseykin <uvsmtid@gmail.com>
2025-10-18 23:09:37 +0800

    Use logger

--- src/local_repo/main/local_repo/cmd_install_homedir.py
+++ src/local_repo/main/local_repo/cmd_install_homedir.py
@@ -4,10 +4,42 @@ import sys
 
 import dotbot
 
+import logging
+
+from protoprimer.primer_kernel import (
+    ColorFormatter,
+    PythonExecutableFilter,
+)
+
+logger = logging.getLogger()
+
+
+# TODO: reuse protoprimer.primer_kernel.configure_stderr_logger:
+def configure_stderr_logger(
+    stderr_log_level: int,
+) -> logging.Handler:
+
+    # Log everything (the filters are supposed to be set on output handlers instead):
+    logger.setLevel(logging.NOTSET)
+
+    stderr_handler: logging.Handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.addFilter(PythonExecutableFilter())
+
+    stderr_formatter = ColorFormatter()
+
+    stderr_handler.setLevel(stderr_log_level)
+    stderr_handler.setFormatter(stderr_formatter)
+
+    logger.addHandler(stderr_handler)
+
+    return stderr_handler
+
 
 def install_homedir(
     ref_root_dir_abs_path: str,
 ):
+
+    configure_stderr_logger(logging.INFO)
 
     homedir_abs_path = os.path.join(
         ref_root_dir_abs_path,
@@ -30,6 +62,6 @@ def install_homedir(
         ]
     )
 
-    print(sys.argv)
+    logger.info(f"sys.argv: {sys.argv}")
 
     dotbot.cli.main()
