a89803024ef312973f1c5eaafd2ce62505e44efc
Alexey Pakseykin <uvsmtid@gmail.com>
2025-10-18 22:22:10 +0800

    Add ./cmd/install_homedir

---
+++ src/local_repo/main/local_repo/cmd_install_homedir.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python3
+import os
+import sys
+
+import dotbot
+
+
+def install_homedir(
+    ref_root_dir_abs_path: str,
+):
+
+    homedir_abs_path = os.path.join(
+        ref_root_dir_abs_path,
+        "homedir",
+    )
+
+    config_abs_path = os.path.join(
+        ref_root_dir_abs_path,
+        "gconf",
+        "install.conf.yaml",
+    )
+
+    sys.argv.extend(
+        [
+            "--base-directory",
+            homedir_abs_path,
+            "--config-file",
+            config_abs_path,
+            "--exit-on-failure",
+        ]
+    )
+
+    print(sys.argv)
+
+    dotbot.cli.main()
